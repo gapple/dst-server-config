25e4009154c4af1a273c740a5434669dd38c3281
Geoff <geoff@gapple.ca>
2017-01-30 13:07:50 -0800

    Create worldgenoverride.lua

---
+++ config/Cluster_1/Caves/worldgenoverride.lua
@@ -0,0 +1,4 @@
+return {
+  override_enabled = true,
+  preset = "DST_CAVE",                   -- "SURVIVAL_TOGETHER", "MOD_MISSING", "SURVIVAL_TOGETHER_CLASSIC", "SURVIVAL_DEFAULT_PLUS", "COMPLETE_DARKNESS", "DST_CAVE", "DST_CAVE_PLUS"
+}
